c7708ffe610ac0f40695ff6218894725d4ee309e
Anand patel <panand054@gmail.com>
2024-06-15 19:30:13 +0530

    first commit

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
+  return (
+    <div className="navbar p-2 flex justify-between items-center">
+      <img src={assets.logo} alt="" className="logo w-[10vw]" />
+      <ul className="navbar-menu flex list-none gap-8 text-lg text-gray-600">
+        <li
+          className={`cursor-pointer ${menu === "home" ? "text-blue-500" : ""}`}
+          onClick={() => setMenu("home")}
+        >
+          Home
+        </li>
+        <li
+          className={`cursor-pointer ${menu === "menu" ? "text-blue-500" : ""}`}
+          onClick={() => setMenu("menu")}
+        >
+          Menu
+        </li>
+        <li
+          className={`cursor-pointer ${
+            menu === "mobile-app" ? "text-blue-500" : ""
+          }`}
+          onClick={() => setMenu("mobile-app")}
+        >
+          Mobile App
+        </li>
+        <li
+          className={`cursor-pointer ${
+            menu === "contact-us" ? "text-blue-500" : ""
+          }`}
+          onClick={() => setMenu("contact-us")}
+        >
+          Contact Us
+        </li>
+      </ul>
+      <div className="navbar-right flex items-center gap-[40px]">
+        <i className="text-3xl ri-search-2-line"></i>
+        <div className="navbar-search-icon relative">
+          <i className="text-3xl ri-shopping-cart-2-fill"></i>
+          <div className="dot absolute min-w-[12px] min-h-[12px] bg-red-500 rounded-[5px] top-[-7px] right-[-6px]"></div>
+        </div>
+        <button className="bg-transparent text-[#49577e] px-4 py-2 border border-blue-500 rounded-md hover:bg-[#fff4f2] hover:text-black transition duration-300 ease-in-out rounded-[50px]">
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
