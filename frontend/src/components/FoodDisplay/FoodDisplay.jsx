6970d94f718a29d71d99ec15ba538f4e08bb3f0f
Anand patel <panand054@gmail.com>
2024-06-18 19:59:43 +0530

    almost foodItem and foodDisplay completed

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,22 +4,21 @@ import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+
   return (
-    <div className="food-display" id="food-display">
-      <h2 className="font-semibold">Top dishes near you</h2>
-      <div className="food-display-list">
-        {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />
-          );
-        })}
+    <div className="food-display mt-30" id="food-display">
+      <h2 className="font-semibold text-3xl">Top dishes near you</h2>
+      <div className="food-display-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6 rounded-md">
+        {food_list.map((item, index) => (
+          <FoodItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </div>
     </div>
   );
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,17 +1,21 @@
 import { assets } from "../../assets/assets";
 
-const FoodItem = ({id, name, price, description, image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
   return (
-    <div className="food-item">
+    <div className="food-item w-full mx-auto rounded-md shadow-md transition duration-300 ease-in-ou">
       <div className="food-item-img-container">
-        <img className="food-item-image" src={image} alt="" />
+        <img
+          className="food-item-image w-full rounded-t-md"
+          src={image}
+          alt=""
+        />
       </div>
-      <div className="food-item-name-rating">
-        <p className="">{name}</p>
-        <img className="" src={assets.rating_starts} alt="" />
-      </div>
-      <div className="food-item-desc">
-        <p className="">{description}</p>
+      <div className="food-item-info p-[20px]">
+        <div className="food-item-name-rating flex justify-between items-center mb-2">
+          <p className="font-extrabold">{name}</p>
+          <img className="" src={assets.rating_starts} alt="" />
+        </div>
+        <p className="text-sm">{description}</p>
         <p className="food-item-price">${price}</p>
       </div>
     </div>
