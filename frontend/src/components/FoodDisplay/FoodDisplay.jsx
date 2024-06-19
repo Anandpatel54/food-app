44913493f799026420dada17afd1bf41e3ab7add
Anand patel <panand054@gmail.com>
2024-06-19 12:23:40 +0530

    category, foodItem and foodDisplay completed

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,16 +9,11 @@ const FoodDisplay = ({ category }) => {
     <div className="food-display mt-30" id="food-display">
       <h2 className="font-semibold text-3xl">Top dishes near you</h2>
       <div className="food-display-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6 rounded-md">
-        {food_list.map((item, index) => (
-          <FoodItem
-            key={index}
-            id={item._id}
-            name={item.name}
-            description={item.description}
-            price={item.price}
-            image={item.image}
-          />
-        ))}
+        {food_list.map((item, index) => {
+           if(category==="All" || category===item.category){
+           return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+          }  
+        })}
       </div>
     </div>
   );
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,22 +1,55 @@
+import { useContext } from "react";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+  
+  const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
+
   return (
     <div className="food-item w-full mx-auto rounded-md shadow-md transition duration-300 ease-in-ou">
-      <div className="food-item-img-container">
+      <div className="food-item-img-container relative">
         <img
           className="food-item-image w-full rounded-t-md"
           src={image}
           alt=""
         />
+        {!cartItems[id] ? (
+          <img
+            className="add absolute bottom-4 right-4 w-8 h-8 rounded-full cursor-pointer"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="food-item-counter flex absolute bottom-4 right-4 rounded-full items-center gap-2 p-2 bg-white text-black">
+            <img
+              className="w-6 h-6 cursor-pointer"
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p className="text-black">{cartItems[id]}</p>
+            <img
+              className="w-6 h-6 cursor-pointer"
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
-      <div className="food-item-info p-[20px]">
+      <div className="food-item-info p-4">
         <div className="food-item-name-rating flex justify-between items-center mb-2">
-          <p className="font-extrabold">{name}</p>
-          <img className="" src={assets.rating_starts} alt="" />
+          <p className="font-semibold">{name}</p>
+          <img className="w-24" src={assets.rating_starts} alt="" />
         </div>
-        <p className="text-sm">{description}</p>
-        <p className="food-item-price">${price}</p>
+        <p className="food-item-desc text-sm text-gray-600 mb-2">
+          {description}
+        </p>
+        <p className="food-item-price text-orange-600 text-lg font-semibold">
+        ₹{price}
+        </p>
       </div>
     </div>
   );
