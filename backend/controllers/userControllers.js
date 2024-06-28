d6af5377ff5bad6c91cffd6153bd7b288ca3479d
Anand patel <panand054@gmail.com>
2024-06-28 12:02:36 +0530

    making userPart

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,4 @@
+const userModel = require("../models/userModel.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    namr: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    cartData: {
+      type: Object,
+      default: {},
+    },
+  },
+  { minimize: false }
+);
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+export default userModel;
